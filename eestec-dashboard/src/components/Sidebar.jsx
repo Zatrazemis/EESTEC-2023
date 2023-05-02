56cf6b1a4c40e5af15143f5da450d6dd890a6eb3
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2023-05-02 13:59:19 +0000

    Created Basic File Structure

--- eestec-dashboard/src/components/Sidebar.jsx
+++ eestec-dashboard/src/components/Sidebar.jsx
@@ -3,23 +3,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <>
-    <div className="z-10 px-10 max-w-xs h-screen bg-gray-800 top-0 rounded-tr-md rounded-br-md fixed ">
-        <div className='my-9 z-50'> 
-            <img src="src/assets/Picture1.png" alt="" />
-        </div>
-
-        <ul className='text-white p-10 font-normal space-y-20 font-serif text-4xl mt-20'>
-            <li className='hover:text-red500'>Dashboard</li>
-            <li>Profile</li>
-            <li>Team</li>
-            <li>Calendar</li>
-            <li>News</li>
-
-        </ul>
-        </div>
-    
-    </>
+ <div>Sidebar</div>
   )
 }
 
