1377be1535ede221bf83cacabcfb74d02892eb77
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2023-05-07 14:45:27 +0300

    Finished Sidebar

--- eestec-dashboard/src/App.jsx
+++ eestec-dashboard/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
 import Topbar from './components/Topbar'
 import Sidebar from './components/Sidebar'
+import { theme } from './theme'
+import 'tailwindcss/tailwind.css'
 
+export const ThemeContext = createContext(theme)
 const App = () => {
   return (<>
+  <ThemeContext.Provider value={theme}>
     <Topbar />
     <Sidebar />
+    </ThemeContext.Provider>
     </>
   )
 }
