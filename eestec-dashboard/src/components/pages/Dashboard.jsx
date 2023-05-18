9135936203af6277e7d7a55ce2738718df235b86
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2023-05-18 16:47:37 +0300

    Added tests

--- eestec-dashboard/src/components/pages/Dashboard.jsx
+++ eestec-dashboard/src/components/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { theme } from '../../theme'
 import { ThemeContext } from '../../App'
+import Weekly from './News/weekly';
+import International from './News/International';
 
 function Dashboard() {
   const theme = useContext(ThemeContext)
@@ -10,6 +12,11 @@ function Dashboard() {
   let w1 = "Lorem Ipsum..."
   let w2 = "Lorem Ipsum..."
   let w3 = "Lorem Ipsum..."
+  let event1 = "Lorem Ipsum..."
+  let event2 = "Lorem Ipsum..."
+  let task1 = "Task1"
+  let task2 = "Task2"
+  let task3 = "Task3"
   return (
     <>
     <div> 
@@ -18,24 +25,21 @@ function Dashboard() {
         <div className="absolute w-56 h-2 top-32 left-5">{date}</div>
         <img src={userPhoto} className="absolute w-48 h-48 right-5" />
       </div>
-      <div className="absolute max-w-3xl w-full bg-white left-72 top-72 h-72 rounded-xl shadow-2xl ">
-        <div className="absolute w-32 h-6 left-7 top-7 font-basic font-normal text-lg font-bold underline">Weekly News</div>
-        <div className="absolute max-w-xl w-full bg-orange h-14 top-20 left-6 rounded-3xl  ">
-          <div className="font-basic ml-8 mt-3 font-semibold text-xl ">{w1}</div>
+      <Weekly />
+      <International /> 
+     <div className="fixed flex right-0 inset-y-28 h-screen overflow-hidden flex justify-center max-w-md bg-white shadow-2xl rounded-2xl w-full ">
+        <div className="bg-gray-300 max-w-sm w-full flex justify-center items-center h-64 mt-10 rounded-2xl ">Calendar</div>
+        <div className="absolute w-44 h-6 top-80  left-8 text-xl font-bold ">Upcoming Events</div>
+        <div className="absolute w-96 h-20 left-8 rounded-xl bg-gray-300 text-xl font-semibold text-center top-96">{event1}</div>
+        <div className="absolute w-96 h-20 left-8 rounded-xl bg-gray-300 text-xl font-semibold text-center top-[485px]">{event2}</div>
+        <div className="absolute w-48 h-6 top-[600px]  left-8 text-xl font-bold ">Uncompleted Tasks</div>
+        <div className="absolute bg-gray-300 max-w-sm w-full text-3xl leading-loose items-center h-48 top-[640px] text-center rounded-2xl ">
+          <ul>
+            <li>{task1}</li>
+            <li>{task2}</li>
+            <li>{task3}</li>
+          </ul>
         </div>
-
-        <div className="absolute max-w-xl w-full bg-pink h-14 top-36 left-6 rounded-3xl  ">
-          <div className="font-basic ml-8 mt-3 font-semibold text-xl ">{w1}</div>
-        </div>
-        <div className="absolute max-w-xl w-full bg-purple h-14 top-52 left-6 rounded-3xl  ">
-          <div className="font-basic ml-8 mt-3 font-semibold text-xl ">{w1}</div>
-        </div>
-        <div className="absolute w-16 h-4 right-5 top-7 underline">
-          <a href="/news">View all</a></div>
-     </div>
-     <div className="fixed right-0 inset-y-64 h-screen overflow-hidden flex justify-center max-w-md bg-white drop-shadow-2xl rounded-xl w-full ">
-        <div className="bg-gray-500 max-w-sm w-full flex justify-center items-center h-64 mt-10 rounded-2xl ">Calendar</div>
-        
      </div>
       </div>
 
