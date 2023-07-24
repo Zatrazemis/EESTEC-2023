883ef739cda4b436ff08db51a917fc39c95f74e3
Zatrazemis <apostoliszatrazemis@gmail.com>
2023-07-24 19:34:36 +0300

    useState Problem

--- eestec-dashboard/src/components/pages/ProfileComp1.jsx
+++ eestec-dashboard/src/components/pages/ProfileComp1.jsx
@@ -11,23 +11,29 @@ export default function ProfileComp1(){
   let toDo3="Lorem ipsum..."
   let toDo4="Lorem ipsum..."
 
-  const [ischecked,setIsChecked]=React.useState(
-    {
-      ISChecked: false
-    }
-  )
+  const [isChecked, setIsChecked] = React.useState({
+    ischecked: false
+  })
 
-  function check(){
-      setIsChecked(previschecked => !previschecked)
-  }
-
-  
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setIsChecked(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }
+    })
+}
 
     return(
         <>
         <div className="bg-orange w-[269px] h-[95px] ml-1 mr-4 mb-8 rounded-[25px] 
                          flex items-center "> 
-          <input type="checkbox" onClick={check} checked={ischecked.ISChecked} className="border-2 border-black rounded-[10px] w-[24px] h-[24px] ml-4 " />
+          <input type="checkbox"  className="border-2 border-black rounded-[10px] w-[24px] h-[24px] ml-4 "  
+                  id="ischecked" 
+                  checked={isChecked.ischecked}
+                  onChange={handleChange}
+                  name="ischecked"/>
          
           <input type="text" placeholder={toDo1} className="ml-4 bg-blue-100 rounded-[5px] w-[200px] h-[35px] " />
         </div>
