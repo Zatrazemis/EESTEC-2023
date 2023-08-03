4df1c6976464c0be2b17ae9cc57f1080c4a22371
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2023-08-03 16:44:01 +0300

    Started working on the Authentication

---
+++ eestec-dashboard/src/components/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Signup() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+  return (
+    
+  )
+}
+
+export default Signup
